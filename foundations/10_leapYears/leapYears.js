f3e5e4676977ccede9b45a52a12f294f9b94bd97
Amin Dana <AminDanaaa@Gmail.com>
2026-06-10 21:07:09 +0330

    exercise done

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    const isYearDivisibleByFour = inputYear % 4 === 0;
+    const isCentury = inputYear % 100 === 0;
+    const isYearDivisibleByFourHundred = inputYear % 400 === 0;
 
+    if (isYearDivisibleByFour &&
+        (!isCentury || isYearDivisibleByFourHundred)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
