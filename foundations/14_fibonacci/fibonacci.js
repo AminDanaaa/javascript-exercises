2e62a104edcf0d9920b760f4297907a99f63ede9
Amin Dana <AminDanaaa@Gmail.com>
2026-06-25 17:21:25 +0330

    exercise done

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(userInput) {
+    let counter;
 
+    if (typeof userInput !== "number") {
+        counter = parseInt(userInput);
+    } else {
+        counter = userInput;
+    }
+
+    if (counter < 0) return "OOPS";
+    if (counter === 0) return 0;
+
+    const fib = [0, 1];
+    for (let i = 2; i <= counter; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[counter];
 };
 
 // Do not edit below this line
