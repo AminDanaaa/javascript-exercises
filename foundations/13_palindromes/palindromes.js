45fdcb93b0d54b4117122ed0422fcb4b7f0e2d7b
Amin Dana <AminDanaaa@Gmail.com>
2026-06-25 17:09:48 +0330

    exercise done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (userInput) {
+    const validChars = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const cleanedString = userInput
+        .toLowerCase()
+        .split("")
+        .filter((character) => validChars.includes(character))
+        .join("");
+
+    const reversedString = cleanedString.split("").reverse().join("");
+
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
